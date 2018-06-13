36d74ca954adce7b6daaa142b231055eaa2cccb7
naojitaniguchi <naoji.taniguchi@gmail.com>
2018-06-14 05:47:35 +0900

    first commit .

---
+++ Assets/RayCastTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTest : MonoBehaviour {
+    public LayerMask LayerMask;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        // if (Input.GetMouseButton(0))
+        {
+            // print("左ボタンが押されている");
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //int mask = 1 << 9 ;
+            //if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+}
